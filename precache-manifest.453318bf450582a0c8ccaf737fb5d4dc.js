088275e20affe00aaad0c96e6952d81e2adea9ca
menglingyu <18629949376@163.com>
2021-06-06 16:32:45 +0000

    Deploying to gh-pages from @ menglingyu/github-actions-demo@fda9f8578f816c282401ee1c2dff0ab2d7432a4a 🚀

---
+++ precache-manifest.453318bf450582a0c8ccaf737fb5d4dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23490f37791cfccd705143a21664f0a",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "3474e4c0ae3ca0c07f92",
+    "url": "/github-actions-demo/static/css/main.8986fa58.chunk.css"
+  },
+  {
+    "revision": "b74240c06e045d881077",
+    "url": "/github-actions-demo/static/js/2.7bc1deec.chunk.js"
+  },
+  {
+    "revision": "3474e4c0ae3ca0c07f92",
+    "url": "/github-actions-demo/static/js/main.eec7fc6d.chunk.js"
+  },
+  {
+    "revision": "41870333dfc813cf444c",
+    "url": "/github-actions-demo/static/js/runtime~main.dd1e6690.js"
+  }
+]);
